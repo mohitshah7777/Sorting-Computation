2e6548fc4302bcaa5b8812f306a6753254cb62ed
Mohit Shah <mohitshah7777@gmail.com>
2021-03-17 15:43:02 +0530

    [Mohit] added compute.sh

--- compute.sh
+++ compute.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+
+declare -A compute
 read -p "Enter 1st Number = " a
 read -p "Enter 2nd Number = " b
 read -p "Enter 3rd Number = " c
@@ -7,3 +9,19 @@ result1=$((a+b*c))
 result2=$((a*b+c))
 result3=$((c+a/b))
 result4=$((a%b+c))
+
+echo "Result 1 = " $result1
+echo "Result 2 = " $result2
+echo "Result 3 = " $result3
+echo "Result 4 = " $result4
+
+compute["1"]=$result1
+compute["2"]=$result2
+compute["3"]=$result3
+compute["4"]=$result4
+
+echo "Results in Dictionary"
+for key in ${!compute[@]}
+do
+        echo $key "=" ${compute[$key]}
+done
